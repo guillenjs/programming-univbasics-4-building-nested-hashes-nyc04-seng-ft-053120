fb06cac66793a7e67f0c3bac9d7b5d90cdb3d992
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-09 23:22:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,11 +10,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  railroads = {
-    piece: "4",
-    rent_in_dollars: {},
-    names: {}
-  }
+key = {railroads: "The :railroads key should point to a hash"}
      
 end
 
