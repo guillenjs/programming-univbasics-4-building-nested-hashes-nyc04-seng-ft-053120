935709b71d1469493d994a4f6efcca438ffaf057
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-09 23:01:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
     {key: "Make sure you have at least one key defined in the monopoly hash",
-    
+    :railroads => {}
       }
 
 end
